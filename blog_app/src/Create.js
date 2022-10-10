41a29fc5fb467dfefa531a510725d14f85d81ab3
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-10-10 22:31:31 +0530

    post_blog

--- blog_app/src/Create.js
+++ blog_app/src/Create.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Kr$na");
+
+  const handleSubmit = (e)  => {
+    e.preventDefault();
+    const blog= {title, body, author}
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add A New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog Auther:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Kr$na">Kr$na</option>
+          <option value="React-Bot">React-Bot</option>
+          <option value="Wiki-Bot">Wiki-Bot</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+      {/* <p>{author}</p> */} 
     </div>
   );
 };
