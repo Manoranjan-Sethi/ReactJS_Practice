9bba3a83bd262de32672efa7287309948dfddf89
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-12-14 11:57:35 +0530

    comps

--- redux/fake-store-redux/src/App.js
+++ redux/fake-store-redux/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import ProductComponent from "./components/ProductComponent";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ redux/fake-store-redux/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <h2>Fake Store</h2>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
